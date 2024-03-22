10c9f0978c327a03e4aa784aaccfc1144664598e
Mike Bamford <mike.bamford@gmail.com>
2024-03-22 20:25:57 +0000

    standardised on `rclone` for the publishing task

--- publish.sh
+++ publish.sh
@@ -10,5 +10,7 @@ touch ./compile.done
 # (we rely on a different flag to `upload` because they can interfere)
 /bin/sh /project/wait.sh ./compile.done
 echo publishing...
+# configure rclone
+/project/rclone.conf.sh
 # upload the book to a S3 bucket publically
-s3cmd --config=/project/.s3cfg put "./compiled.pdf" "$1" --acl-public
+rclone copyto "./compiled.pdf" "$1"
